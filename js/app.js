ef8cdc27f0aee4df9db8d1dcd59191d4621c5fce
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-04 21:20:56 -0500

    solo falta un poco

--- js/app.js
+++ js/app.js
@@ -1,8 +1,17 @@
 $(document).ready(function(){
-    $('li').on('click', function(){
-      $(this).addClass('active')
-    })
-    $('a').on('mouseover',function(){
-      $(this).addClass('active')
-    })
+  $('li').click(function(){
+    $(this).addClass('active a:after')  
+  })
+  $('li').mouseover(function(){
+    $(this).removeClass('active a:after')  
+  })
+  $('a').mouseover(function(){
+    $(this).addClass('active')
+  })
+  $('a').click(function(){
+    $(this).addClass('active')
+  })
 });
+
+
+
