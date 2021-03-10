f99fd3967339e8d3dae372a5ce94a90046f2ca6b
LuisMiguelRG <luis.miguel.dj22@gmail.com>
2021-03-09 19:17:13 -0500

    restauro archivos

--- js/app.js
+++ js/app.js
@@ -2,16 +2,14 @@ $(document).ready(function(){
   $('li').click(function(){
     $(this).addClass('active')  
   })
-  $('li').mouseleave(function(){
-    $(this).removeClass('active')  
-  })
-  $('a').mouseenter(function(){
+  $('a').mouseover(function(){
     $(this).addClass('active')
   })
   $('a').mouseleave(function(){
     $(this).removeClass('active')
   })
 });
+
 
 
 
